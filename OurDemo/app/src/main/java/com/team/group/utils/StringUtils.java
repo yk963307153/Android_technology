490b3467ad773dc511ab0a5cd1833fe1bc028ca7
lijiafu <warwg2.@163.com>
2016-11-03 18:13:23 +0800

    调整

--- OurDemo/app/src/main/java/com/team/group/utils/StringUtils.java
+++ OurDemo/app/src/main/java/com/team/group/utils/StringUtils.java
@@ -1,6 +1,7 @@
 package com.team.group.utils;
 
 import java.io.UnsupportedEncodingException;
+import java.util.regex.Pattern;
 
 /**
  * <pre>
@@ -11,10 +12,13 @@ import java.io.UnsupportedEncodingException;
  * </pre>
  */
 public class StringUtils {
+    private final static Pattern emailer = Pattern
+            .compile("\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*");
 
     private StringUtils() {
         throw new UnsupportedOperationException("u can't instantiate me...");
     }
+
 
     /**
      * 判断字符串是否为null或长度为0
@@ -345,5 +349,128 @@ public class StringUtils {
             sb.append(py);
         }
         return sb.toString();
+    }
+
+    /**
+     * 判断是不是一个合法的电子邮件地址
+     *
+     * @param email
+     * @return
+     */
+    public static boolean isEmail(String email) {
+        if (email == null || email.trim().length() == 0)
+            return false;
+        return emailer.matcher(email).matches();
+    }
+
+    /**
+     * 字符串转整数
+     *
+     * @param str
+     * @param defValue
+     * @return
+     */
+    public static int toInt(String str, int defValue) {
+        try {
+            return Integer.parseInt(str);
+        } catch (Exception e) {
+        }
+        return defValue;
+    }
+
+    /**
+     * 对象转整数
+     *
+     * @param obj
+     * @return 转换异常返回 0
+     */
+    public static int toInt(Object obj) {
+        if (obj == null)
+            return 0;
+        return toInt(obj.toString(), 0);
+    }
+
+    /**
+     * 对象转整数
+     *
+     * @param obj
+     * @return 转换异常返回 0
+     */
+    public static long toLong(String obj) {
+        try {
+            return Long.parseLong(obj);
+        } catch (Exception e) {
+        }
+        return 0;
+    }
+
+    /**
+     * 字符串转布尔值
+     *
+     * @param b
+     * @return 转换异常返回 false
+     */
+    public static boolean toBool(String b) {
+        try {
+            return Boolean.parseBoolean(b);
+        } catch (Exception e) {
+        }
+        return false;
+    }
+
+    /**
+     * 将字符串中间用星号隐藏
+     *
+     * @param start 从哪一位开始隐藏
+     * @param end   最后剩几位
+     */
+    public static String hideString(String str, int start, int end) {
+        if (start < 0 || end < 0) {
+            return str;
+        } else if ((start + end) > str.length()) {
+            return str;
+        } else {
+            int length = str.length();
+            String strStart = str.substring(0, start);
+            String strMid = str.substring(start, length - end);
+            String strEnd = str.substring(start + strMid.length(), length);
+            StringBuffer s = new StringBuffer();
+            for (int i = 0; i < strMid.length(); i++) {
+                s.append("*");
+            }
+            return strStart + s.toString() + strEnd;
+        }
+    }
+
+    /**
+     * 判断调用toString方法的对象是不是null，避免造成空指针异常
+     *
+     * @param
+     * @return 如果传递的参数是null，返回空串，否则返回obj.toString()
+     * <p>
+     * null可以理解为原始类型至于可以把null作为参数只是特殊规定
+     */
+    public static String object2String(Object obj) {
+        String str = obj == null ? "" : obj.toString();
+        return str;
+    }
+
+    /**
+     * 判断给定字符串是否空白串。 空白串是指由空格、制表符、回车符、换行符组成的字符串 若输入字符串为null或空字符串，返回true
+     *
+     * @param input
+     * @return boolean
+     */
+    public static boolean isEmpty(String input) {
+        if (input == null || "".equals(input))
+            return true;
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c != ' ' && c != '\t' && c != '\r' && c != '\n') {
+                return false;
+            }
+        }
+        return true;
     }
 }
