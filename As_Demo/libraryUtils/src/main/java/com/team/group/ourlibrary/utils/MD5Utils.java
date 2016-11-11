43d9c5c401ee35a6d1efcbf9a3a34313ce9d081c
lijiafu <warwg2.@163.com>
2016-11-11 09:14:33 +0800

    11.11      -_-

--- As_Demo/libraryUtils/src/main/java/com/team/group/ourlibrary/utils/MD5Utils.java
+++ As_Demo/libraryUtils/src/main/java/com/team/group/ourlibrary/utils/MD5Utils.java
@@ -9,6 +9,9 @@ import java.security.NoSuchAlgorithmException;
  */
 
 public class MD5Utils {
+
+    private static final char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
     //另一种写法
 //    public  static String getMD5Convert(String str){
 //        String password = null;
@@ -87,5 +90,30 @@ public class MD5Utils {
         }
         return "";
     }
+
+    public static String sha1(byte[] bs) {
+        try {
+            MessageDigest o = MessageDigest.getInstance("SHA1");
+            o.update(bs);
+            byte[] r = o.digest();
+            return toHex(r);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static String toHex(byte[] bs) {
+        int j = bs.length;
+        char rs[] = new char[j * 2];
+        int k = 0;
+        for (int i = 0; i < j; i++) {
+            byte byte0 = bs[i];
+            rs[k++] = hexDigits[byte0 >>> 4 & 0xf];
+            rs[k++] = hexDigits[byte0 & 0xf];
+        }
+        return new String(rs);
+    }
+
 
 }
