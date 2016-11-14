9b766639b8b69f9ebc91df08f35bef4a5bf44a56
lijiafu <warwg2.@163.com>
2016-11-14 16:54:44 +0800

    +力

--- As_Demo/app/src/main/java/com/team/group/activity/MainActivity.java
+++ As_Demo/app/src/main/java/com/team/group/activity/MainActivity.java
@@ -47,6 +47,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    /**
+     * 侧滑
+     */
     private void initNavigationView() {
 
         toolbar.setBackgroundColor(ThemeUtils.getToolBarColor());
